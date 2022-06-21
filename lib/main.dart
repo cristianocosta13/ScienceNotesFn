fe3b7838773cf04bb221162826b4514a744cb725
cristianocosta13 <103438234+cristianocosta13@users.noreply.github.com>
2022-06-20 22:25:49 -0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:sciencenotes/pages/inicial_page.dart';
+void main() {
+  runApp(
+    const MaterialApp(
+      home: InicialPage(),
+    ),
+  );
+}
