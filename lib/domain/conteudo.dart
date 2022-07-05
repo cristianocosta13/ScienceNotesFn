78e252678f94236a178881a094f7dee200e7121a
cristianocosta13 <103438234+cristianocosta13@users.noreply.github.com>
2022-07-05 09:00:25 -0300

    Update conteudo.dart

--- lib/domain/conteudo.dart
+++ lib/domain/conteudo.dart
@@ -1,9 +1,13 @@
+import 'package:flutter/material.dart';
+
 class Conteudo {
   final String imagem;
   final String titulo;
   final String resumo;
   final String aplicacao;
   final String equacao;
+  final Color corBotao;
+  final Color corConteudo;
 
 
   Conteudo({
@@ -12,5 +16,7 @@ class Conteudo {
     required this.resumo,
     required this.aplicacao,
     required this.equacao,
+    required this.corBotao,
+    required this.corConteudo,
   });
 }
