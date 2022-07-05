85931258dcf379621a5887d9a611636569d00139
cristianocosta13 <103438234+cristianocosta13@users.noreply.github.com>
2022-07-05 08:59:06 -0300

    Update fisica_page.dart

--- lib/pages/fisica_page.dart
+++ lib/pages/fisica_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:sciencenotes/widgets/card_conteudo.dart';
-import 'package:sciencenotes/domain/conteudo.dart';
-import 'package:sciencenotes/widgets/lista_conteudo.dart';
+import 'package:siencenotes/widgets/card_conteudo.dart';
+import 'package:siencenotes/domain/conteudo.dart';
+import 'package:siencenotes/widgets/lista_conteudo.dart';
 
 class FisicaPage extends StatefulWidget {
   const FisicaPage({Key? key}) : super(key: key);
@@ -12,6 +12,8 @@ class FisicaPage extends StatefulWidget {
 
 class _FisicaPageState extends State<FisicaPage> {
   Conteudo conteudoF1 = Conteudo(
+    corConteudo: Colors.blue.shade100,
+    corBotao: Colors.blue.shade200,
     imagem: 'https://i.ibb.co/NWGhwy1/1655756102311.png',
     titulo: 'Cinemática',
     resumo: 'A cinemática é a área da mecânica que realiza os estudos dos movimentos, sem levar em consideração os motivos para que estes ocorram.',
@@ -19,6 +21,8 @@ class _FisicaPageState extends State<FisicaPage> {
     equacao: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."',
   );
   Conteudo conteudoF2 = Conteudo(
+    corConteudo: Colors.blue.shade100,
+    corBotao: Colors.blue.shade200,
     imagem: 'https://i.ibb.co/kB1zXTv/1655757349218.png',
     titulo: 'Dinâmica',
     resumo: 'Dinâmica, uma das áreas que compõem a mecânica, é a parte da física que realiza o estudo das causas do movimento, de maneira a realizar a análise e descrever as forças que são responsáveis por produzi-las.',
@@ -26,6 +30,8 @@ class _FisicaPageState extends State<FisicaPage> {
     equacao: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."',
   );
   Conteudo conteudoF3 = Conteudo(
+    corConteudo: Colors.blue.shade100,
+    corBotao: Colors.blue.shade200,
     imagem: 'https://i.ibb.co/xmncRFb/1655757553840.png',
     titulo: 'Estática',
     resumo: 'Estática é a parte da física, mais especificamente da mecânica, que realiza o estudo dos corpos que não se movimentam, ou seja, que estão em repouso.',
@@ -33,6 +39,8 @@ class _FisicaPageState extends State<FisicaPage> {
     equacao: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."',
   );
   Conteudo conteudoF4 = Conteudo(
+    corConteudo: Colors.blue.shade100,
+    corBotao: Colors.blue.shade200,
     imagem: 'https://i.ibb.co/S5MNh20/1655757687571.png',
     titulo: 'Hidrostática e Hidrodinâmica',
     resumo: 'Hidrostática é a parte da física que realiza o estudo dos fluidos em repouso, já hidrodinâmica realiza o estudo desses fluidos em movimento.',
@@ -40,6 +48,8 @@ class _FisicaPageState extends State<FisicaPage> {
     equacao: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."',
   );
   Conteudo conteudoF5 = Conteudo(
+    corConteudo: Colors.blue.shade100,
+    corBotao: Colors.blue.shade200,
     imagem: 'https://i.ibb.co/r6YLXzf/1655757858876.png',
     titulo: 'Termologia',
     resumo: 'Termologia é um ramo da Física que estuda os fenômenos térmicos como calor, temperatura, dilatação, energia térmica, estudo térmico dos gases etc.',
@@ -47,6 +57,9 @@ class _FisicaPageState extends State<FisicaPage> {
     equacao: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."',
   );
   Conteudo conteudoF6 = Conteudo(
+
+    corConteudo: Colors.blue.shade100,
+    corBotao: Colors.blue.shade200,
     imagem: 'https://i.ibb.co/r309HRp/1655762704824.png',
     titulo: 'Óptica',
     resumo: 'Óptica é a parte da Física que estuda fenômenos associados à luz. Podendo ser dividida em óptica geométrica e óptica física, de acordo com a forma que a luz se comporta.',
@@ -54,6 +67,9 @@ class _FisicaPageState extends State<FisicaPage> {
     equacao: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."',
   );
   Conteudo conteudoF7 = Conteudo(
+
+    corConteudo: Colors.blue.shade100,
+    corBotao: Colors.blue.shade200,
     imagem: 'https://i.ibb.co/MBN8RVw/1655761475200.png',
     titulo: 'Ondulatória',
     resumo: 'Ondulatória é a parte da Física que estuda as ondas, ou seja, qualquer perturbação (pulso) que se propaga em um meio.',
@@ -61,6 +77,9 @@ class _FisicaPageState extends State<FisicaPage> {
     equacao: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."',
   );
   Conteudo conteudoF8 = Conteudo(
+
+    corConteudo: Colors.blue.shade100,
+    corBotao: Colors.blue.shade200,
     imagem: 'https://i.ibb.co/nsnDktn/1655761574377.png',
     titulo: 'Eletrostática',
     resumo: 'Eletrostática é o ramo da eletricidade que estuda as propriedades e o comportamento de cargas elétricas em repouso.',
@@ -68,6 +87,9 @@ class _FisicaPageState extends State<FisicaPage> {
     equacao: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."',
   );
   Conteudo conteudoF9 = Conteudo(
+
+    corConteudo: Colors.blue.shade100,
+    corBotao: Colors.blue.shade200,
     imagem: 'https://i.ibb.co/RjJH62M/1655761710933.png',
     titulo: 'Eletrodinâmica',
     resumo: 'A eletrodinâmica é a parte da Física que estuda as cargas elétricas em movimento, ou seja, a dinâmica da eletricidade.',
@@ -75,6 +97,9 @@ class _FisicaPageState extends State<FisicaPage> {
     equacao: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."',
   );
   Conteudo conteudoF10 = Conteudo(
+
+    corConteudo: Colors.blue.shade100,
+    corBotao: Colors.blue.shade200,
     imagem: 'https://i.ibb.co/Xt7HVyv/1655761815569.png',
     titulo: 'Eletromagnetismo',
     resumo: 'O eletromagnetismo é a parte da Física que estuda a eletricidade e o magnetismo, bem como as relações estabelecidas entre eles.',
@@ -93,8 +118,9 @@ class _FisicaPageState extends State<FisicaPage> {
           style: TextStyle(fontSize: 24,color: Colors.white,  fontFamily: 'Staatliches'),
         ),
       ),
-      backgroundColor: Color(0xFFDFE9F5),
+      backgroundColor: Colors.blue.shade50,
       body: Container(
+        color: Colors.blue.shade50,
         child: ListaConteudo(
           conteudo1: conteudoF1, conteudo2: conteudoF2, conteudo3: conteudoF3,
           conteudo4: conteudoF4, conteudo5: conteudoF5, conteudo6: conteudoF6,
